01b51e39076a386b49633ca39fee383a14185387
Miguel <miguel@bisect.pt>
2018-10-16 13:34:14 +0100

    Enhances tests

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,8 +1,16 @@
 #include <cstdlib>
 #include <iostream>
 
+#include <mongoc.h>
+#include <bson.h>
+
 int main()
 {
+    mongoc_init();
+    mongoc_client_t *client = NULL;
+
+    mongoc_cleanup ();
+
     std::cout << "Bincrafters\n";
     return EXIT_SUCCESS;
 }
