781422e629058547c1955039b95ffb4d6795ea80
Jan-Grimo Sobez <jan-grimo.sobez@phys.chem.ethz.ch>
2020-02-17 13:34:34 +0100

    Add ICU dependency
    Previously, ICU use was autodetected and not part of package id

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
-from conans import ConanFile, CMake, tools
 import os
+from conans import ConanFile, CMake, tools
 
 
 class MongoCDriverConan(ConanFile):
@@ -13,16 +13,17 @@ class MongoCDriverConan(ConanFile):
     exports = ["LICENSE.md"]
     exports_sources = ["Find*.cmake", "header_path.patch", "CMakeLists.txt"]
     generators = "cmake"
-    
+
     settings = "os", "compiler", "arch", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {'shared': False, 'fPIC': True}
-    
+    options = {"shared": [True, False], "fPIC": [
+        True, False], "icu": [True, False]}
+    default_options = {"shared": False, "fPIC": True, "icu": True}
+
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
-    
+
     requires = 'zlib/1.2.11'
-    
+
     def configure(self):
         # Because this is pure C
         del self.settings.compiler.libcxx
@@ -36,23 +37,29 @@ class MongoCDriverConan(ConanFile):
         if not tools.os_info.is_macos and not tools.os_info.is_windows:
             self.requires.add("openssl/1.0.2u")
 
+        if self.options.icu:
+            self.requires.add("icu/63.1@bincrafters/stable")
+
     def source(self):
         tools.get("https://github.com/mongodb/mongo-c-driver/releases/download/{0}/mongo-c-driver-{0}.tar.gz".format(self.version),
                   sha256="ad479a6d3499038ec19ca80a30dfa99277644bb884e424362935b06e2d5f7988")
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
-        #tools.patch(base_path=self._source_subfolder, patch_file="header_path.patch")
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["ENABLE_TESTS"] = False
-        cmake.definitions["ENABLE_EXAMPLES"] = False
-        cmake.definitions["ENABLE_AUTOMATIC_INIT_AND_CLEANUP"] = False
-        cmake.definitions["ENABLE_BSON"] = "ON"
-        cmake.definitions["ENABLE_SASL"] = "OFF"
-        cmake.definitions["ENABLE_STATIC"] = "OFF" if self.options.shared else "ON"
-        cmake.definitions["ENABLE_SHM_COUNTERS"] = "OFF"
+        additional_definitions = {
+            "ENABLE_TESTS": False,
+            "ENABLE_EXAMPLES": False,
+            "ENABLE_AUTOMATIC_INIT_AND_CLEANUP": False,
+            "ENABLE_BSON": "ON",
+            "ENABLE_SASL": "OFF",
+            "ENABLE_SHM_COUNTERS": "OFF",
+            "ENABLE_STATIC": "OFF" if self.options.shared else "ON",
+            "ENABLE_ICU": "ON" if self.options.icu else "OFF",
+        }
 
+        cmake.definitions.update(additional_definitions)
         cmake.configure(build_folder=self._build_subfolder)
 
         return cmake
@@ -62,7 +69,8 @@ class MongoCDriverConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy(pattern="COPYING*", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="COPYING*", dst="licenses",
+                  src=self._source_subfolder)
         self.copy("Find*.cmake", ".", ".")
 
         # cmake installs all the files
@@ -70,13 +78,20 @@ class MongoCDriverConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ['mongoc-1.0', 'bson-1.0'] if self.options.shared \
-            else ['mongoc-static-1.0', 'bson-static-1.0']
+        if self.options.shared:
+            self.cpp_info.libs = ['mongoc-1.0', 'bson-1.0']
+        else:
+            self.cpp_info.libs = ['mongoc-static-1.0', 'bson-static-1.0']
+
         self.cpp_info.includedirs = [os.path.join("include", "libmongoc-1.0"),
                                      os.path.join("include", "libbson-1.0")]
 
+        if self.options.icu and not self.options.shared:
+            self.cpp_info.libs.extend(self.deps_cpp_info["icu"].libs)
+
         if tools.os_info.is_macos:
-            self.cpp_info.exelinkflags = ['-framework CoreFoundation', '-framework Security']
+            self.cpp_info.exelinkflags = [
+                '-framework CoreFoundation', '-framework Security']
             self.cpp_info.sharedlinkflags = self.cpp_info.exelinkflags
 
         if tools.os_info.is_linux:
@@ -89,4 +104,5 @@ class MongoCDriverConan(ConanFile):
                 self.cpp_info.libs.append('resolv')
 
             if tools.os_info.is_windows:
-                self.cpp_info.libs.extend(['ws2_32.lib', 'secur32.lib', 'crypt32.lib', 'BCrypt.lib', 'Dnsapi.lib'])
+                self.cpp_info.libs.extend(
+                    ['ws2_32.lib', 'secur32.lib', 'crypt32.lib', 'BCrypt.lib', 'Dnsapi.lib'])
