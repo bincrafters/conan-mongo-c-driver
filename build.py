66aa139ad4a5323ba76d7ff68b327eec99227710
Jan-Grimo Sobez <jan-grimo.sobez@phys.chem.ethz.ch>
2020-02-18 09:58:36 +0100

    Add build missing

--- build.py
+++ build.py
@@ -5,5 +5,8 @@
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
-    builder = build_template_default.get_builder(pure_c=True)
+    builder = build_template_default.get_builder(
+        pure_c=True,
+        build_policy="missing"
+    )
     builder.run()
