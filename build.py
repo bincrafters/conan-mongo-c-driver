54b73316299662f36eefd5febacb36cd8a02086e
Jan-Grimo Sobez <jan-grimo.sobez@phys.chem.ethz.ch>
2020-02-18 11:48:48 +0100

    Remove build missing

--- build.py
+++ build.py
@@ -5,8 +5,5 @@
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
-    builder = build_template_default.get_builder(
-        pure_c=True,
-        build_policy="missing"
-    )
+    builder = build_template_default.get_builder(pure_c=True)
     builder.run()
